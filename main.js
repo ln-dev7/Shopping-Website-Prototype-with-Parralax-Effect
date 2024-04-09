f944150d3f70fea3aa009b11d14e84edd763b17c
LN <leonelngoya@gmail.com>
2024-04-09 19:36:31 +0100

    init

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
-import './style.scss'
-import gsap from 'gsap'
+import "./style.scss";
+import gsap from "gsap";
+import { CustomEase } from "gsap/all";
+
+const photos = document.querySelectorAll(".photo");
+const backgrounds = document.querySelectorAll(".background");
+const texts = document.querySelectorAll(".text");
+
+const arrowNext = document.querySelector(".arrow-next");
+const arrowPrev = document.querySelector(".arrow-prev");
+
+// duration:2.5,
+// ease: CustomEase.create("custom", "M0,0 C0.69,0 0,1.01 1,1 "),
+
+let currentSlide = 0;
